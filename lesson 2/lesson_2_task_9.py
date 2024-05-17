679ae88029d7939cfa7673053896c545dc07d7db
dDarthFalcon <154631631+dDarthFalcon@users.noreply.github.com>
2024-05-18 00:16:50 +0300

    add second homework

---
+++ lesson 2/lesson_2_task_9.py
@@ -0,0 +1,16 @@
+#Вариант 1
+var_1 = 37
+var_2 = 99
+lst = [var_1, var_2]
+var_2 = lst[0]
+var_1 = lst [1]
+print (var_1)
+print (var_2)
+
+
+#Вариант 2
+var_1 = 37
+var_2 = 99
+var_1, var_2 = var_2, var_1
+print(var_1)
+print(var_2)
