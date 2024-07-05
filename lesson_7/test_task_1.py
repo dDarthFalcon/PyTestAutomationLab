9f85bf22d688f5c239daee2916d7e9a903bc84f4
dDarthFalcon <154631631+dDarthFalcon@users.noreply.github.com>
2024-07-05 20:58:29 +0300

    Update test_task_1.py
    Код отредактирован с учетом замечания наставка: лучше передавать параметры формы из теста, а не писать в коде страницы

--- lesson_7/test_task_1.py
+++ lesson_7/test_task_1.py
@@ -3,10 +3,23 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.FormPage import FormPage
 
+# Данные формы
+first_name = 'Иван'
+last_name = 'Петров'
+address = 'Ленина, 55-3'
+city = 'Москва'
+country = 'Россия'
+zipcode = ''
+email = 'test@skypro.com'
+phone = '+7985899998787'
+job_position = 'QA'
+company = 'SkyPro'
+
+
 def test_fill_form():
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     form_page = FormPage(browser)
-    form_page.fill_form()
+    form_page.fill_form(first_name, last_name, address, city, country, zipcode, email, phone, job_position, company)
     form_page.submit_form()
     fill_fields = form_page.get_fill_fields()
     expected_fields = ['first-name', 'last-name', 'address', 'city', 'country', 'e-mail', 'phone', 'job-position', 'company']
@@ -15,7 +28,7 @@ def test_fill_form():
 def test_blank_form():
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     form_page = FormPage(browser)
-    form_page.fill_form()
+    form_page.fill_form(first_name, last_name, address, city, country, zipcode, email, phone, job_position, company)
     form_page.submit_form()
     blank_fields = form_page.get_blank_fields()
     expected_fields = ["zip-code"]
